73d8b56a5b691b9d8cc6dbfba4b2a2301c6011e2
Jia Hao <jiahao5776@gmail.com>
2021-08-12 14:15:47 -0700

    completed variable exercises

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+var firstName = "Jia";
+let lastName= "Chen";
+const age = 20;
+
+firstName = "potatoes";
+lastName = "potatoes";
+
+console.log('Hello ' + firstName + ' ' + lastName + '! You are ' + age + ' years old!');
